8a2ab56b4bb8112aad706e97ac5651d323256faf
Ben Blank <ben.blank@gmail.com>
2022-07-15 14:20:58 -0700

    Add more tests.

---
+++ tests/suites/alias-output-formatting-empty.py
@@ -0,0 +1,123 @@
+from dataclasses import replace
+
+from testlib import (
+    CommandOutput,
+    TestExecutionContext,
+    TestCase,
+    TestSuite,
+    common_parameters,
+    format_parameters,
+    get_parameter_matrix,
+)
+
+
+def get_test_suite() -> TestSuite:
+    tests = []
+
+    for parameters in get_parameter_matrix(
+        {
+            parameter: common_parameters[parameter]
+            for parameter in ["command-alias", "location-flags"]
+        }
+    ):
+        context = TestExecutionContext()
+
+        # Also used to construct the "default" test case.
+        shell_flag = TestCase(
+            "--shell flag",
+            context,
+            [*parameters["command-alias"], *parameters["location-flags"], "--shell"],
+            exit_code=0,
+            output="",
+        )
+
+        # Also used to construct the "--config" test case.
+        config_header_flags = TestCase(
+            "--config --header flags",
+            context,
+            [
+                *parameters["command-alias"],
+                *parameters["location-flags"],
+                "--config",
+                "--header",
+            ],
+            exit_code=0,
+            output=CommandOutput(stdout="[alias]\n", stderr=""),
+        )
+
+        # Also used to construct the "--json" test case.
+        json_pretty_flags = TestCase(
+            "--json --pretty flags",
+            context,
+            [
+                *parameters["command-alias"],
+                *parameters["location-flags"],
+                "--json",
+                "--pretty",
+            ],
+            exit_code=0,
+            output=CommandOutput(stdout="{}\n", stderr=""),
+        )
+
+        tests.append(
+            TestSuite(
+                f"with parameters {format_parameters(parameters)}",
+                [
+                    replace(
+                        shell_flag,
+                        name="default",
+                        command_line=[
+                            *parameters["command-alias"],
+                            *parameters["location-flags"],
+                        ],
+                    ),
+                    shell_flag,
+                    replace(
+                        config_header_flags,
+                        name="--config flag",
+                        command_line=[
+                            *parameters["command-alias"],
+                            *parameters["location-flags"],
+                            "--config",
+                        ],
+                    ),
+                    config_header_flags,
+                    TestCase(
+                        "--config --no-header flags",
+                        context,
+                        [
+                            *parameters["command-alias"],
+                            *parameters["location-flags"],
+                            "--config",
+                            "--no-header",
+                        ],
+                        exit_code=0,
+                        output="",
+                    ),
+                    replace(
+                        json_pretty_flags,
+                        name="--json flag",
+                        command_line=[
+                            *parameters["command-alias"],
+                            *parameters["location-flags"],
+                            "--json",
+                        ],
+                    ),
+                    json_pretty_flags,
+                    TestCase(
+                        "--json --compact flags",
+                        context,
+                        [
+                            *parameters["command-alias"],
+                            *parameters["location-flags"],
+                            "--json",
+                            "--compact",
+                        ],
+                        exit_code=0,
+                        output=CommandOutput(stdout="{}", stderr=""),
+                    ),
+                ],
+            )
+        )
+
+    return TestSuite("alias: output formatting (no aliases defined)", tests)
---
+++ tests/suites/unalias.py
@@ -0,0 +1,76 @@
+from typing import Callable, Sequence
+from testlib import (
+    TestCase,
+    TestExecutionContext,
+    TestSuite,
+    add_aliases,
+    clear_aliases,
+    common_parameters,
+    format_parameters,
+    get_parameter_matrix,
+)
+
+
+ALIASES = {"foo": "diff", "ml": "!echo foo\necho bar", "func": "!f() {}; f"}
+
+
+def after_each(
+    context: TestExecutionContext, location_flags: Sequence[str]
+) -> Callable[[], None]:
+    def after_each_impl() -> None:
+        clear_aliases(context, location_flags)
+
+    return after_each_impl
+
+
+def before_each(
+    context: TestExecutionContext, location_flags: Sequence[str]
+) -> Callable[[], None]:
+    def before_each_impl() -> None:
+        add_aliases(context, location_flags, ALIASES)
+
+    return before_each_impl
+
+
+def get_test_suite() -> TestSuite:
+    context = TestExecutionContext()
+
+    return TestSuite(
+        "unalias: smoke tests",
+        [
+            TestSuite(
+                f"with parameters {format_parameters(parameters)}",
+                [
+                    TestCase(
+                        "doesn't produce an error",
+                        context,
+                        [
+                            *parameters["command-unalias"],
+                            *parameters["location-flags"],
+                            "foo",
+                        ],
+                        exit_code=0,
+                    ),
+                    TestCase(
+                        "doesn't produce an error with --dry-run",
+                        context,
+                        [
+                            *parameters["command-unalias"],
+                            *parameters["location-flags"],
+                            "--dry-run",
+                            "foo",
+                        ],
+                        exit_code=0,
+                    ),
+                ],
+                before_each=before_each(context, parameters["location-flags"]),
+                after_each=after_each(context, parameters["location-flags"]),
+            )
+            for parameters in get_parameter_matrix(
+                {
+                    parameter: common_parameters[parameter]
+                    for parameter in ["command-unalias", "location-flags"]
+                }
+            )
+        ],
+    )
